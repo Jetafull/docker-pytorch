2b439cc826750487b9f1ba39ffdf45b379890b70
jetafull <jetafull@gmail.com>
2018-06-17 14:25:41 +0900

    add dockerfile to create tensorflow + pytorch

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -itd --name pytorch04 -v /home/linlin/Downloads/:/home/downloads -v /media/homes/zwickyfu/database/:/home/database --runtime=nvidia --net=host jetafull/pytorch:v0.4-cuda9.1 bash
+docker run -itd --name pytorch04 -v /home/linlin/Downloads/:/home/downloads -v /media/WD_4TB/database/:/home/database --runtime=nvidia --net=host jetafull/deeplearning:cuda9.0 bash
