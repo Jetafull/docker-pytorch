1d41ccf5788216d26c2b7976eb152fc73dcfef24
jetafull <jetafull@gmail.com>
2019-04-10 22:35:38 +0900

    Added cuda-10.1

---
+++ cuda-10.1/docker_build.sh
@@ -0,0 +1,2 @@
+docker build -t jetafull/deeplearning:cuda10.1 .
+#docker push jetafull/deeplearning:cuda9.0
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -itd --name pytorch04 -v /home/linlin/Downloads/:/home/downloads -v /media/WD_4TB/database/:/home/database --runtime=nvidia --net=host jetafull/deeplearning:cuda9.0 bash
+docker run -itd --name pytorch1_0 -v /home/linlin/Downloads/:/home/downloads -v /media/WD_4TB/database/:/home/database --runtime=nvidia --net=host jetafull/deeplearning:cuda10.1 bash
